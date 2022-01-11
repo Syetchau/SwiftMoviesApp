a8b4e4bb351f42a490694a536a3f619c7c094666
syetchau95 <syetchau@hotmail.com>
2022-01-11 16:35:22 +0800

    Initial Commit

---
+++ Movies/MoviesApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoviesApp.swift
+//  Movies
+//
+//  Created by LiewSyetChau on 11/1/22.
+//
+
+import SwiftUI
+
+@main
+struct MoviesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
